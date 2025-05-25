32bc0f935329bc74334c5da4cda7e1f2ac496a0b
valentino <126835108+Valen89hh@users.noreply.github.com>
2025-05-25 08:27:55 -0500

    add load dotenv

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
+from dotenv import load_dotenv
 import os
+
+# Cargar variables desde el archivo .env
+load_dotenv()
 
 TOKEN = os.environ.get("TOKEN")
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
 
-print(TELEGRAM_TOKEN, CHAT_ID, TOKEN)
+print(TELEGRAM_TOKEN, CHAT_ID, TOKEN, SS)
