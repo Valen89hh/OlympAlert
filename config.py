25a6cd5dd793a1bb5af5043a6e13cbe4fe042c4a
valentino <126835108+Valen89hh@users.noreply.github.com>
2025-05-25 08:37:20 -0500

    fix numpy

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ TOKEN = os.environ.get("TOKEN")
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
 
-print(TELEGRAM_TOKEN, CHAT_ID, TOKEN, SS)
+print(TELEGRAM_TOKEN, CHAT_ID, TOKEN)
